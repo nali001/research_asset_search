f4e3d30bfcc5d9cd6bf629cac504ebfc8f35b640
Na MC6 <n.li@uva.nl>
2022-09-14 17:06:24 +0200

    Fix the Response data

--- search_engine_app/notebook_search/apis.py
+++ search_engine_app/notebook_search/apis.py
@@ -5,12 +5,19 @@ from rest_framework.decorators import api_view
 
 import json
 @api_view(['GET'])
-def welcome(request): 
+def welcome(request) -> Response: 
+    ''' Return the welcome message to API connection. 
+    Args: 
+        request: Received request from the client. 
+
+    Returns: 
+        Response to the client.
+    '''
     if request.method == 'GET':
-        return Response(json.dumps({'name': 'notebook_search API', 'message': 'Congratulations! You have succefully received information from notebook_search API :)'}))
+        return Response({'name': 'notebook_search API', 'message': 'Congratulations! You have succefully received information from notebook_search API :)'})
 
 @api_view(['GET'])
 def api_test(request):
     if request.method == 'GET':
         print('GETTTTTTTTTTTT:', request.GET)
-        return Response(json.dumps({'message': request.data}))
+        return Response({'message': request.data})
