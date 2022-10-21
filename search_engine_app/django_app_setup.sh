5577697702145e1d186c156a1bf2d6b325949c30
Na MC6 <n.li@uva.nl>
2022-10-21 15:08:43 +0200

    Modify settings

--- search_engine_app/django_app_setup.sh
+++ search_engine_app/django_app_setup.sh
@@ -1,6 +1,8 @@
+# change the settings to production
+copy ./search_engine_app/settings_prod.py ./search_engine_app/settings.py
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
-copy ./search_engine_app/settings_prod.py ./search_engine_app/settings.py
-# change the settings
+
 gunicorn --bind :7777 --workers 4 search_engine_app.wsgi:application
