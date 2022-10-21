bd56e5291f1ae5c305449d8d835624232cfe9384
Na MC6 <n.li@uva.nl>
2022-10-21 15:06:55 +0200

    Modify the settings for deployment

--- search_engine_app/django_app_setup.sh
+++ search_engine_app/django_app_setup.sh
@@ -1,4 +1,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
+copy ./search_engine_app/settings_prod.py ./search_engine_app/settings.py
+# change the settings
 gunicorn --bind :7777 --workers 4 search_engine_app.wsgi:application
