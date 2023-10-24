403d89e58bb3a7a39fb7c1cce59e03441ed7eb0d
Na MC6 <n.li@uva.nl>
2023-10-24 22:10:13 +0200

    restore the static file settings

--- search_engine_app/search_engine_app/urls.py
+++ search_engine_app/search_engine_app/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path('api/', include('apis.urls')),
     path('api/docs/schema', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+]
 
